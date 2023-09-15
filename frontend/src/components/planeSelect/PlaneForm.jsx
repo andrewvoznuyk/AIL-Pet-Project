6f2e1f3345b311f484265fb9e4bcc5bad6940325
Konoval Mykola <mr.kolyakonoval@gmail.com>
2023-09-15 20:01:57 +0300

    Plane form refactored

--- frontend/src/components/planeSelect/PlaneForm.jsx
+++ frontend/src/components/planeSelect/PlaneForm.jsx
@@ -1,6 +1,16 @@
-import {useContext, useEffect, useState} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import axios from "axios";
-import {Breadcrumbs, FormControl, InputLabel, Link, MenuItem, Select, TextField, Typography} from "@mui/material";
+import {
+    Breadcrumbs,
+    Button,
+    FormControl,
+    InputLabel,
+    Link,
+    MenuItem,
+    Select,
+    TextField,
+    Typography
+} from "@mui/material";
 import {responseStatus} from "../../utils/consts";
 import userAuthenticationConfig from "../../utils/userAuthenticationConfig";
 
@@ -8,13 +18,12 @@ const PlaneForm = ({currentPlane}) => {
 
     return (
         <>
-            <Typography gutterBottom variant="h5" component="div">
-                {currentPlane.plane}
-            </Typography>
-            <Typography gutterBottom variant="h5" component="div">
-                {currentPlane.brand}
-            </Typography>
+            <TextField label="Brand" id="Brand" type="text" name="Brand" defaultValue={currentPlane.brand} style={{margin:10}} required/>
+            <TextField label="Name" id="Name" type="text" name="Name" defaultValue={currentPlane.plane} style={{margin:10}} required/>
+            <TextField label="Engine" id="Engine" type="text" name="Engine" defaultValue={currentPlane.engine} style={{margin:10}} required/>
+            <TextField label="Passenger count" type="number" id="price[gte]" name="price[gte]" defaultValue={currentPlane.passenger_capacity} style={{margin:10}}/>
             <img src={currentPlane.imgThumb} alt=""/>
+            <Button type="submit" style={{margin:10}}>Add Plane</Button>
         </>
     );
 };
