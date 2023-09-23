c017b533614215881dd0ac974cc8ab7742a17587
Andrew Vozniuk <iipz224_vav@student.ztu.edu.ua>
2023-09-23 16:43:42 +0300

    Remade CalculateTicketPriceService.php. - It is working correctly. - Logic has been changed. - Nothing to change.

--- api/src/Services/CalculateTicketPriceService.php
+++ api/src/Services/CalculateTicketPriceService.php
@@ -2,36 +2,34 @@
 
 namespace App\Services;
 
-
-use App\Entity\Flight;
+use App\Entity\Ticket;
+use DateTime;
 use Exception;
 
 class CalculateTicketPriceService
 {
-
-    private GetMilesService $getMilesService;
-
-    public function __construct(GetMilesService $getMilesService)
-    {
-        $this->getMilesService = $getMilesService;
-    }
-
+    private const FLUE_PRICE = 0.4;
+    private const PRICE_INCREASE = 0.1;
 
     /**
-     * @param Flight $flight
-     * @return int
      * @throws Exception
      */
-    public function calculateTicketPrice(Flight $flight) : int
+    public function calculateTicketPrice(Ticket $ticket) : float
     {
-        $departureAirportId = $flight->getFromLocation()->getAirport();
-        $arrivalAirportId = $flight->getToLocation()->getAirport();
+        $km = $ticket->getFlight()->getDistance();
+        $classCoef = $ticket->getFlight()->getPlacesCoefs();
+        $class = $ticket->getClass();
 
-        $km =  $this->getMilesService->getMilesFromCityAtoCityB($departureAirportId, $arrivalAirportId);
+        $nowDateTime = new DateTime();
+        $departureDateTime = $ticket->getFlight()->getDeparture();
+        $interval = $nowDateTime->diff($departureDateTime);
 
-        $price = $km * 0.5;
+        $initPrice = $ticket->getFlight()->getInitPrices()[$class];
 
+        $price = $km * self::FLUE_PRICE * (1 / (1 + $interval->days * self::PRICE_INCREASE)) * (1 + $classCoef[0][$class]);
+        if ($price < $initPrice)
+            $price = $initPrice;
 
-        return  0;
+        return  $price;
     }
 }
