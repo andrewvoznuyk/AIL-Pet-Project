4e49d838f26fcb44ee428251f4993272709bc04b
Nadiia Melnychenko <nadiia092330@gmail.com>
2023-09-24 16:32:10 +0300

    Show User fullname
    - user can see name in profile
    - nothing to fix

---
+++ api/src/Controller/GetUserNameController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
+
+class GetUserNameController extends AbstractController
+{
+
+    /**
+     * @var Security
+     */
+    private Security $security;
+
+    /**
+     * @param Security $security
+     */
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
+    /**
+     * @return JsonResponse
+     */
+    #[Route('/username', name: 'get_username', methods: "GET")]
+    public function index(): JsonResponse
+    {
+        $user = $this->security->getUser();
+
+        if (!$user) {
+            return $this->json([
+                'message' => 'missing credentials',
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+
+        $name = $user->getName();
+        $surname = $user->getSurname();
+
+        return $this->json([
+            'name'    => $name,
+            'surname' => $surname,
+        ]);
+    }
+
+}
